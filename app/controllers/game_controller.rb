dc43ae4a95ccb6b32a96a8283e4cacae55498fc0
Eoin McMillan <twitter: @mceoin>
2014-06-30 19:31:39 -0700

    naming conventions changed from *welcome* to *game* -- working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,35 +1,31 @@
 class GameController < ApplicationController
 # @@c = 0
 
-   @board = Array.new(9)
+@board = Array.new(9)
 
-	def index
-     @board = Array.new(9)
-      render :index
-       
-
-    end
+def index
+ @board = Array.new(9)
+ render :index
 
 
+end
+
+def create
+
+ click =  params["user_click"].to_i
+
+ if click == 9
+   @board = []
+   @winner = nil
+   Game.resetboard
+   render :index
+ else
+  @board = Game.cell(click)
+  @winner = Game.winner
+  render :index
+end
+
+end
 
 
-   def create
-
-   	   
-	    click =  params["user_click"].to_i
-
-	    if click == 9
-         @board = []
-         @winner = nil
-         Game.resetboard
-
-	   else
-	     @board = Game.cell(click)
-	     @winner = Game.winner
-	       render :index
-	   end 
-         
-	 end
-
- 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,11 @@
-class Welcome
+class Game
 
 	@@board = Array.new(9)
 	@@counter = 0
 	@@winner = nil
 
   def self.resetboard
-  	
+
      @@board = []
      @@winner = nil
      @@counter = 0
@@ -15,61 +15,61 @@ class Welcome
 
 
      def self.cell(cellvalue)
-     
-     	
+
+
      	if @@counter % 2 == 0
      		if @@board[cellvalue] == nil
-     			
+
      	  @@board[cellvalue] = "X"
      	  @@counter+=1
-     	
+
      	end
      	else
      		if @@board[cellvalue] == nil
      	  @@board[cellvalue] = "O"
-     
+
      	  @@counter+=1
-     	 
+
      	end
      	end
-     
-       
+
+
         @@board
      end
 
 
      def self.winner
      	if ([@@board[0],@@board[1],@@board[2]].uniq.length == 1 && [@@board[0],@@board[1],@@board[2]].uniq[0] != nil)
-         
+
      	  @@winner = "Player #{@@board[0]} WINS!"
-     	  
+
 
      	elsif ([@@board[3],@@board[4],@@board[5]].uniq.length == 1 && [@@board[3],@@board[4],@@board[5]].uniq[0] != nil)
-     	 
+
      	   @@winner = "Player #{@@board[3]} WINS!"
-           
+
      	 elsif ([@@board[6],@@board[7],@@board[8]].uniq.length == 1 && [@@board[6],@@board[7],@@board[8]].uniq[0] != nil)
-     	 
+
      	  @@winner = "Player #{@@board[6]} WINS!"
-            
+
      	   elsif ([@@board[0],@@board[3],@@board[6]].uniq.length == 1 && [@@board[0],@@board[3],@@board[6]].uniq[0] != nil)
-     	   
+
      	  @@winner = "Player #{@@board[0]} WINS!"
-           
+
      	   elsif ([@@board[1],@@board[4],@@board[7]].uniq.length == 1 && [@@board[1],@@board[4],@@board[7]].uniq[0] != nil)
-     	  
+
      	  @@winner = "Player #{@@board[1]} WINS!"
-          
+
      	   elsif ([@@board[2],@@board[5],@@board[8]].uniq.length == 1 && [@@board[2],@@board[5],@@board[8]].uniq[0] != nil)
-     	   	
+
      	   @@winner = "Player #{@@board[2]} WINS!"
-        
+
      	   elsif ([@@board[0],@@board[4],@@board[8]].uniq.length == 1 &&  [@@board[0],@@board[4],@@board[8]].uniq[0] != nil)
-     	  
+
      	  @@winner = "Player #{@@board[0]} WINS!"
-         
+
      	   elsif ([@@board[2],@@board[4],@@board[6]].uniq.length == 1 && [@@board[2],@@board[4],@@board[6]].uniq[0] != nil)
-     	   
+
      	  @@winner = "Player #{@@board[2]} WINS!"
 
           elsif (@@board.compact.length == 9)
@@ -82,8 +82,8 @@ class Welcome
 
      # 	@@board.each_slice(3){|x| p}
      # end
-     	 
 
-     	 	
+
+
 
  end
